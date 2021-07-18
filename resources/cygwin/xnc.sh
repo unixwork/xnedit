380350b0c009f1964e00ac04e27ce9c6ac136a3a
Valerio <efa@iol.it>
2021-07-18 22:27:52 +0200

    changed cygwin root to 'cygroot', moved fonts inside usr/share, added share/X11/locale to solve non-ASCII char input missing, created 'tmp' to solve "bash.exe: warning: could not find /tmp", added dependencies check at the begin of xnedit_pkg

--- resources/cygwin/xnc.sh
+++ resources/cygwin/xnc.sh
@@ -1,32 +1,33 @@
 #!/bin/sh
+# xnc.sh: Copyright 2021 Valerio Messina v0.02.0 2021/07/18 GNU GPL v2+
 # this script create the environment to run XNEdit from Cygwin
 echo "Starting 'xnc.sh' ..."
-echo Bash param:$1
+echo "Bash param:$1"
 
 #pwd
 export XNEditDir=`pwd`
-echo PATH=$PATH
+echo "PATH=$PATH"
 # to find the cygwin1.dll, shell commands, X libraries
-export PATH="$XNEditDir/usr/bin:$PATH"
-echo PATH=$PATH
+export PATH="$XNEditDir/cygroot/bin:$PATH"
+echo "PATH=$PATH"
 
 #if (test "" = "$PROGRAMFILES") then
 #   export PROGRAMFILES="D:\installer"
 #fi
-#echo PROGRAMFILES=$PROGRAMFILES
-#export ProgramFiles=`./usr/bin/cygpath -u "$PROGRAMFILES"`
-#echo ProgramFiles=$ProgramFiles
+#echo "PROGRAMFILES=$PROGRAMFILES"
+#export ProgramFiles=`./cygroot/bin/cygpath -u "$PROGRAMFILES"`
+#echo "ProgramFiles=$ProgramFiles"
 
 # specify a home directory, XNEdit needs it to store its preference files
 # maybe used also to find fonts directory as specified in 'fonts.conf'
-echo HOME=$HOME
+echo "HOME=$HOME"
 #export HOME="$ProgramFiles/xnedit_64bit"
 #export HOME=/cygdrive/d/installer/xnedit_64bit
 if (test "" = "$HOME") then
    #export HOME=`cygpath -H`/$USERNAME
    export HOME=`cygpath -u $USERPROFILE`
 fi
-echo HOME=$HOME
+echo "HOME=$HOME"
 
 # this is a new variable. when set, XNEdit stores its preference files
 # in this directory under the new names `nedit.rc' (previous name `.nedit')
@@ -34,29 +35,29 @@ echo HOME=$HOME
 #export XNEDIT_HOME="$ProgramFiles/xnedit_64bit"
 #export XNEDIT_HOME=/cygdrive/d/installer/xnedit_64bit
 export XNEDIT_HOME=$XNEditDir/.xnedit
-echo XNEDIT_HOME=$XNEDIT_HOME
+echo "XNEDIT_HOME=$XNEDIT_HOME"
 
 # to find the display
 #export DISPLAY=localhost:0.0
 export DISPLAY=:0
-echo DISPLAY=$DISPLAY
+echo "DISPLAY=$DISPLAY"
 
 # for the keyboard, isn't always necessary
-#export XKEYSYMDB="$ProgramFiles/xnedit_64bit/cygwin/bin/xkeysymdb"
+#export XKEYSYMDB="$ProgramFiles/xnedit_64bit/cygroot/bin/xkeysymdb"
 #export XKEYSYMDB=/cygdrive/d/installer/xnedit_64bit/xkeysymdb
 export XKEYSYMDB=$XNEditDir/xkeysymdb
-echo XKEYSYMDB=$XKEYSYMDB
+#echo "XKEYSYMDB=$XKEYSYMDB"
 
 # used to find 'fonts.conf'
 #export FONTCONFIG_PATH="/cygdrive/d/installer/xnedit_64bit"
 export FONTCONFIG_PATH=$XNEditDir
-echo FONTCONFIG_PATH=$FONTCONFIG_PATH
+echo "FONTCONFIG_PATH=$FONTCONFIG_PATH"
 
 # fonts.conf: <dir prefix="xdg">fonts</dir>
 # fonts are loaded from $XDG_DATA_HOME/fonts
 #export XDG_DATA_HOME="/cygdrive/d/installer/xnedit_64bit"
-export XDG_DATA_HOME=$XNEditDir
-echo XDG_DATA_HOME=$XDG_DATA_HOME
+export XDG_DATA_HOME=$XNEditDir/cygroot/usr/share
+echo "XDG_DATA_HOME=$XDG_DATA_HOME"
 
 export LANG=C
 export LC_ALL=C
@@ -77,9 +78,9 @@ then
   # letters, but XNEdit sees a difference internally, the file name and
   # path get translated to all lower-case
   filename=`cygpath -u "$filename" | tr A-Z a-z`
-  echo Run Xnedit with a parameter ...
+  echo "Run Xnedit with a parameter ..."
   xnedit "$filename" &
 else
-  echo Run Xnedit without parameters ...
+  echo "Run Xnedit without parameters ..."
   xnedit &
 fi
