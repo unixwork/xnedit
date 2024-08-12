5b2170f78caec95b5c5de40c1287936d14a09530
Olaf Wintermann <olaf.wintermann@gmail.com>
2024-08-12 20:24:15 +0200

    more getfiles.c cleanup

--- util/getfiles.c
+++ util/getfiles.c
@@ -102,112 +102,8 @@ static XmString DefaultDirectory = NULL;
 
 static char* DefaultDirectoryStr = NULL;
 
-/* User settable option for leaving the file name text field in
-   GetExistingFilename dialogs.  Off by default so new users will get
-   used to typing in the list rather than in the text field */
-static int RemoveRedundantTextField = True;
-
-/* Text for help button help display */
-/* ... needs variant for VMS */
-#ifndef SGI_CUSTOM
-static const char *HelpExist =
-"The file open dialog shows a list of directories on the left, and a list \
-of files on the right.  Double clicking on a file name in the list on the \
-right, or selecting it and pressing the OK button, will open that file.  \
-Double clicking on a directory name, or selecting \
-it and pressing \"Filter\", will move into that directory.  To move upwards in \
-the directory tree, double click on the directory entry ending in \"..\".  \
-You can also begin typing a file name to select from the file list, or \
-directly type in directory and file specifications in the \
-field labeled \"Filter\".\n\
-\n\
-If you use the filter field, remember to include \
-either a file name, \"*\" is acceptable, or a trailing \"/\".  If \
-you don't, the name after the last \"/\" is interpreted as the file name to \
-match.  When you leave off the file name or trailing \"/\", you won't see \
-any files to open in the list \
-because the filter specification matched the directory file itself, rather \
-than the files in the directory.";
-
-static const char *HelpNew = 
-"This dialog allows you to create a new file, or to save the current file \
-under a new name.  To specify a file \
-name in the current directory, complete the name displayed in the \"Save File \
-As:\" field near the bottom of the dialog.  If you delete or change \
-the path shown in the field, the file will be saved using whatever path \
-you type, provided that it is a valid Unix file specification.\n\
-\n\
-To replace an existing file, select it from the Files list \
-and press \"OK\", or simply double click on the name.\n\
-\n\
-To save a file in another directory, use the Directories list \
-to move around in the file system hierarchy.  Double clicking on \
-directory names in the list, or selecting them and pressing the \
-\"Filter\" button will select that directory.  To move upwards \
-in the directory tree, double \
-click on the directory entry ending in \"..\".  You can also move directly \
-to a directory by typing the file specification of the path in the \"Filter\" \
-field and pressing the \"Filter\" button.";
-
-#else /* SGI_CUSTOM */
-static const char *HelpExist =
-"The \"File to Edit:\" field shows a list of directories and files in the \
-current directory.\n\
-\n\
-Double clicking on a file name in the list, or selecting it and pressing \
-the OK button, will open that file.\n\
-\n\
-Double clicking on a directory name, or selecting it and pressing the OK \
-button will move into that directory.  To navigate upwards in the file \
-system hierarchy you can use the buttons above the \"Selection\" field  \
-(each of these buttons represent a directory level). \n\
-\n\
-You can also enter a file or directory name to open in the field \
-labeled \"Selection\".  Pressing the space bar will complete a partial file \
-name, or beep if no files match.  The drop pocket to the right of the field \
-will accept icons dragged from the desktop, and the button with the circular \
-arrows, to the right, of the field recalls previously selected \
-directories.\n\
-\n\
-The \"Filter\" button allows you to narrow down the list of files and \
-directories shown in the \"File to Edit:\" field.  The default filter of \
-\"*\" allows all files to be listed.";
-
-static const char *HelpNew = 
-"This dialog allows you to create a new file or to save the current file \
-under a new name.\n\
-\n\
-To specify a file name in the current directory, complete the name displayed \
-in the \"Save File As:\" field.  If you delete or change the path shown \
-in the field, the file will be saved using whatever path you type, provided \
-that it is a valid Unix file specification.\n\
-\n\
-To replace an existing file, select it from the \"Files\" list and press \
-\"OK\", or simply double click on the name in the \"Files\" list.\n\
-\n\
-To save a file in another directory, use the \"Files\" list to move around \
-in the file system hierarchy.  Double clicking on a directory name, or \
-selecting it and pressing the OK button, will move into that directory. \
-To navigate upwards in the file system hierarchy you can use the buttons \
-above the \"Selection\" field (each of these buttons represent a directory \
-level).\n\
-\n\
-You can also move directly to a directory by typing the file specification \
-of the path in the \"Save File As:\" field.  Pressing the space bar will \
-complete a partial directory or file \
-name, or beep if nothing matches.  The drop pocket to the right of the field \
-will accept icons dragged from the desktop, and the button with the circular \
-arrows, to the right, of the field recalls previously selected \
-directories.\n\
-\n\
-The \"Filter\" button allows you to narrow down the list of files and \
-directories shown in the \"Files\" field.  The default filter of \
-\"*\" allows all files to be listed.";
-#endif /* SGI_CUSTOM */
-
 /*                    Local Callback Routines and variables                */
 
-static void newHelpCB(Widget w, Widget helpPanel, caddr_t call_data);
 static void createYesNoDialog(Widget parent);
 static void createErrorDialog(Widget parent);
 static int  doYesNoDialog(const char *msg);
@@ -215,16 +111,9 @@ static void doErrorDialog(const char *errorString, const char *filename);
 static void existOkCB(Widget w, Boolean * client_data,
 	              XmFileSelectionBoxCallbackStruct *call_data);
 static void existCancelCB(Widget w, Boolean * client_data, caddr_t call_data);
-static void existHelpCB(Widget w, Widget helpPanel, caddr_t call_data);
 static void errorOKCB(Widget w, caddr_t client_data, caddr_t call_data);
 static void yesNoOKCB(Widget w, caddr_t client_data, caddr_t call_data);
 static void yesNoCancelCB(Widget w, caddr_t client_data, caddr_t call_data);
-static Widget createPanelHelp(Widget parent, const char *text, const char *title);
-static void helpDismissCB(Widget w, Widget helpPanel, caddr_t call_data);
-static void makeListTypeable(Widget listW);
-static void listCharEH(Widget w, XtPointer callData, XEvent *event,
-	Boolean *continueDispatch);
-static int compareXmStrings(const void *string1, const void *string2);
 
 static int  SelectResult = GFN_CANCEL;  /*  Initialize results as cancel   */
 static Widget YesNoDialog;		/* "Overwrite?" dialog widget	   */
@@ -233,47 +122,6 @@ static Widget ErrorDialog;		/* Dialog widget for error msgs	   */
 static int ErrorDone;			/* Flag to mark dialog completed   */
 static void (*OrigDirSearchProc)();	/* Built in Motif directory search */
 static void (*OrigFileSearchProc)();	/* Built in Motif file search proc */
-
-/* 
- * Do the hard work of setting up a file selection dialog
- */
-Widget getFilenameHelper(Widget parent, char *promptString, char *filename, 
-        int existing) 
-{
-    int       n;                      /* number of arguments               */
-    Arg	      args[MAX_ARGS];	      /* arg list	                   */
-    Widget    fileSB;	              /* widget file select box 	   */
-    XmString  titleString;	      /* compound string for dialog title  */
-
-    n = 0;
-    titleString = XmStringCreateSimple(promptString);
-    XtSetArg(args[n], XmNdialogStyle, XmDIALOG_FULL_APPLICATION_MODAL); n++;
-    XtSetArg(args[n], XmNdialogTitle, titleString); n++;
-    fileSB = CreateFileSelectionDialog(parent,"FileSelect",args,n);
-    XmStringFree(titleString);
-#ifndef SGI_CUSTOM
-    if (existing && RemoveRedundantTextField)
-        XtUnmanageChild(XmFileSelectionBoxGetChild(fileSB, XmDIALOG_TEXT)); 
-    XtUnmanageChild(XmFileSelectionBoxGetChild(fileSB, XmDIALOG_SELECTION_LABEL));
-
-    XtVaSetValues(XmFileSelectionBoxGetChild(fileSB, XmDIALOG_FILTER_LABEL),
-            XmNmnemonic, 'l',
-            XmNuserData, XmFileSelectionBoxGetChild(fileSB, XmDIALOG_FILTER_TEXT),
-            NULL);
-    XtVaSetValues(XmFileSelectionBoxGetChild(fileSB, XmDIALOG_DIR_LIST_LABEL),
-            XmNmnemonic, 'D',
-            XmNuserData, XmFileSelectionBoxGetChild(fileSB, XmDIALOG_DIR_LIST),
-            NULL);
-    XtVaSetValues(XmFileSelectionBoxGetChild(fileSB, XmDIALOG_LIST_LABEL),
-            XmNmnemonic, promptString[strspn(promptString, "lD")],
-            XmNuserData, XmFileSelectionBoxGetChild(fileSB, XmDIALOG_LIST),
-            NULL);
-    AddDialogMnemonicHandler(fileSB, FALSE);
-    RemapDeleteKey(XmFileSelectionBoxGetChild(fileSB, XmDIALOG_FILTER_TEXT));
-    RemapDeleteKey(XmFileSelectionBoxGetChild(fileSB, XmDIALOG_TEXT));
-#endif
-    return fileSB;
-}
 
 /*  GetExistingFilename				  	                   */
 /*									   */
@@ -481,11 +329,6 @@ static void yesNoOKCB(Widget w, caddr_t client_data, caddr_t call_data)
     YesNoResult = ynYes;
 }
 
-static void existHelpCB(Widget w, Widget helpPanel, caddr_t call_data)
-{
-    ManageDialogCenteredOnPointer(helpPanel);
-}
-
 static void errorOKCB(Widget w, caddr_t client_data, caddr_t call_data)
 {
     ErrorDone = True;
@@ -494,154 +337,6 @@ static void errorOKCB(Widget w, caddr_t client_data, caddr_t call_data)
 static void yesNoCancelCB(Widget w, caddr_t client_data, caddr_t call_data)
 {
     YesNoResult = ynNo;
-}
-
-static Widget createPanelHelp(Widget parent, const char *helpText, const char *title)
-{
-    Arg al[20];
-    int ac;
-    Widget form, text, button;
-    XmString st1;
-    
-    ac = 0;
-    form = CreateFormDialog(parent, "helpForm", al, ac);
-
-    ac = 0;
-    XtSetArg (al[ac], XmNbottomAttachment, XmATTACH_FORM);  ac++;
-    XtSetArg (al[ac], XmNtopAttachment, XmATTACH_NONE);  ac++;
-    XtSetArg(al[ac], XmNlabelString, st1=XmStringCreateLtoR ("OK", 
-                      XmSTRING_DEFAULT_CHARSET)); ac++;
-    XtSetArg (al[ac], XmNmarginWidth, BUTTON_WIDTH_MARGIN);  ac++;
-    button = XmCreatePushButtonGadget(form, "ok", al, ac);
-    XtAddCallback(button, XmNactivateCallback, (XtCallbackProc)helpDismissCB,
-    	    (char *)form);
-    XmStringFree(st1);
-    XtManageChild(button);
-    SET_ONE_RSRC(form, XmNdefaultButton, button);
-    
-    ac = 0;
-    XtSetArg(al[ac], XmNrows, 15);  ac++;
-    XtSetArg(al[ac], XmNcolumns, 60);  ac++;
-    XtSetArg(al[ac], XmNresizeHeight, False);  ac++;
-    XtSetArg(al[ac], XmNtraversalOn, False); ac++;
-    XtSetArg(al[ac], XmNwordWrap, True);  ac++;
-    XtSetArg(al[ac], XmNscrollHorizontal, False);  ac++;
-    XtSetArg(al[ac], XmNeditMode, XmMULTI_LINE_EDIT);  ac++;
-    XtSetArg(al[ac], XmNeditable, False);  ac++;
-    XtSetArg(al[ac], XmNvalue, helpText);  ac++;
-    XtSetArg(al[ac], XmNtopAttachment, XmATTACH_FORM);  ac++;
-    XtSetArg(al[ac], XmNleftAttachment, XmATTACH_FORM);  ac++;
-    XtSetArg(al[ac], XmNbottomAttachment, XmATTACH_WIDGET);  ac++;
-    XtSetArg(al[ac], XmNrightAttachment, XmATTACH_FORM);  ac++;
-    XtSetArg(al[ac], XmNbottomWidget, button);  ac++;
-    text = XmCreateScrolledText(form, "helpText", al, ac);
-    AddMouseWheelSupport(text);
-    XtManageChild(text);
-    
-    SET_ONE_RSRC(XtParent(form), XmNtitle, title);
-    
-    return form;
-}
-
-static void helpDismissCB(Widget w, Widget helpPanel, caddr_t call_data)
-{
-    XtUnmanageChild(helpPanel);
-}
-
-/*
-** Add ability for user to type filenames to a list widget
-*/
-static void makeListTypeable(Widget listW)
-{
-    XtAddEventHandler(listW, KeyPressMask, False, listCharEH, NULL);
-}
-
-/*
-** Action procedure for processing characters typed in a list, finds the
-** first item matching the characters typed so far.
-*/
-static int nKeystrokes = 0; /* Global key stroke history counter */
-static void listCharEH(Widget w, XtPointer callData, XEvent *event,
-	Boolean *continueDispatch)
-{
-    char charString[5], c, *itemString;
-    int nChars, nItems, i, cmp, selectPos, topPos, nVisible;
-    XmString *items;
-    KeySym kSym;
-    char name[MAXPATHLEN], path[MAXPATHLEN];
-    static char keystrokes[MAX_LIST_KEYSTROKES];
-    static Time lastKeyTime = 0;
-    
-    /* Get the ascii character code represented by the event */
-    nChars = XLookupString((XKeyEvent *)event, charString, sizeof(charString),
-    	    &kSym, NULL);
-    c = charString[0];
-    
-    /* Process selected control keys, but otherwise ignore the keystroke
-       if it isn't a single printable ascii character */
-    *continueDispatch = False;
-    if (kSym==XK_BackSpace || kSym==XK_Delete) {
-    	nKeystrokes = nKeystrokes > 0 ? nKeystrokes-1 : 0;
-    	return;
-    } else if (kSym==XK_Clear || kSym==XK_Cancel || kSym==XK_Break) {
-    	nKeystrokes = 0;
-    	return;
-    } else if (nChars!=1 || c<0x021 || c>0x07e) {
-    	*continueDispatch = True;
-    	return;
-    }
-    
-    /* Throw out keystrokes and start keystroke accumulation over from 
-       scratch if user waits more than MAX_LIST_KESTROKE_WAIT milliseconds */
-    if (((XKeyEvent *)event)->time - lastKeyTime > MAX_LIST_KESTROKE_WAIT)
-    	nKeystrokes = 0;
-    lastKeyTime = ((XKeyEvent *)event)->time;
-    	
-    /* Accumulate the current keystroke, just beep if there are too many */
-    if (nKeystrokes >= MAX_LIST_KEYSTROKES)
-    	XBell(XtDisplay(w), 0);
-    else
-#ifdef VMS
-    	keystrokes[nKeystrokes++] = toupper(c);
-#else
-    	keystrokes[nKeystrokes++] = c;
-#endif
-    
-    /* Get the items (filenames) in the list widget */
-    XtVaGetValues(w, XmNitems, &items, XmNitemCount, &nItems, NULL);
-    
-    /* compare them with the accumulated user keystrokes & decide the
-       appropriate line in the list widget to select */
-    selectPos = 0;
-    for (i=0; i<nItems; i++) {
-    	XmStringGetLtoR(items[i], XmSTRING_DEFAULT_CHARSET, &itemString);
-    	if (ParseFilename(itemString, name, path) != 0) {
-	   NEditFree(itemString);
-	   return;
-	}
-	NEditFree(itemString);
-    	cmp = strncmp(name, keystrokes, nKeystrokes);
-    	if (cmp == 0) {
-    	    selectPos = i+1;
-    	    break;
-    	} else if (cmp > 0) {
-    	    selectPos = i;
-    	    break;
-    	}
-    }
-
-    /* Make the selection, and make sure it will be visible */
-    XmListSelectPos(w, selectPos, True);
-    if (selectPos == 0) /* XmListSelectPos curiously returns 0 for last item */
-    	selectPos = nItems + 1;
-    XtVaGetValues(w, XmNtopItemPosition, &topPos,
-    	    XmNvisibleItemCount, &nVisible, NULL);
-    if (selectPos < topPos)
-    	XmListSetPos(w, selectPos-2 > 1 ? selectPos-2 : 1);
-    else if (selectPos > topPos+nVisible-1)
-    	XmListSetBottomPos(w, selectPos+2 <= nItems ? selectPos+2 : 0);
-    /* For LessTif 0.89.9. Obsolete now? */
-    XmListSelectPos(w, selectPos, True);
 }
 
 
