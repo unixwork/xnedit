495eaf89378b6a8d7f2a5fbe4ca26b71335ad946
Olaf Wintermann <olaf.wintermann@gmail.com>
2025-06-04 20:11:27 +0200

    update contributors list

--- source/help_data.h
+++ source/help_data.h
@@ -2,7 +2,7 @@
 *                                                                              *
 * help_data.h --  Nirvana Editor help module data                              *
 *                                                                              *
-                 Generated on May 8, 2025 (Do NOT edit!)
+                 Generated on Jun 4, 2025 (Do NOT edit!)
                  Source of content from file help.etx
 *                                                                              *
 * Copyright (c) 1999-2025 Mark Edel                                            *
@@ -5137,7 +5137,8 @@ static char * htxt_version [] = {
 "Additional NEdit patches by: Fredrik Jönsson, Per Grahn, Peter Kovář. ",
 "\n\n",
 "XNEdit contributions: Laszlo Ersek, Mike Becker, Peter Mühlenpfordt, ",
-"Valerio Messina, Jack Zeal, Chris Wareham and Alexander Hajnal. ",
+"Valerio Messina, Jack Zeal, Chris Wareham, Alexander Hajnal and ",
+"Paul Uiterlinden. ",
 "\n\n",
 "The regular expression matching routines used in XNEdit are adapted (with ",
 "permission) from original code written by Henry Spencer at the ",
--- source/help_topic.h
+++ source/help_topic.h
@@ -2,7 +2,7 @@
 *                                                                              *
 * help_topic.h --  Nirvana Editor help display                                 *
 *                                                                              *
-                 Generated on May 8, 2025 (Do NOT edit!)
+                 Generated on Jun 4, 2025 (Do NOT edit!)
                  Source of content from file help.etx
 *                                                                              *
 * Copyright (c) 1999-2025 Mark Edel                                            *
